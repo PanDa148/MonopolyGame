fc4b3f5a9b442645600cd2951915a9cef39fb207
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-06-29 14:45:29 -0400

    Update Monopoly.h

--- Monopoly.h
+++ Monopoly.h
@@ -2,29 +2,32 @@
 #include "Space.h"
 #include "Player.h"
 #include "Card.h"
+#include "Land.h"
+#include "Airport.h"
+#include "Service_Provider.h"
 #include <vector>
 #include <string>
+
 class Monopoly{
+    private:
     std::vector<Player> players;
     std::vector<Space*> spaces;
     std::vector<Card*> community_chest_cards;
     std::vector<Card*> chance_cards;
-    public:
-    Monopoly();
-    void assign_players();
-    void end_game();
-    void assemble_game_board();
+
     void assemble_cards();
     int move_player(Player& player,int num);
     void players_turn(Player& player);
-    void manage_player_turns();
     void take_mortgage(Player& player);
     bool is_bankrupt(Player& player);
     std::string player_who_went_bankrupt();
     void show_properties_owned(Player& player);
     int show_options(Player& player);
-  //void buy_sell_property (Property* property, int direction);
- /* bool has_property (std::string property_name);
-  Bool has_monopoly (std::string color);
-  Void buy_sell_residences (Land& land, int num, std::string type);*/
+
+    public:
+    Monopoly();
+    void assign_players();
+    void assemble_game_board();
+    void end_game();
+    void manage_player_turns();
 };
