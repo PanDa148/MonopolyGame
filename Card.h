26ae624051b5fd1036b7841190769113d7582849
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-06-29 14:39:51 -0400

    Update Card.h

--- Card.h
+++ Card.h
@@ -11,14 +11,14 @@ class Card{
 	public:
 	Card();
 	Card(std::string new_text);
-	virtual void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+	//virtual void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
-
+/*
 class Collect : public Card{
 	int amount;
 	public:
 	Collect (std::string new_text, int new_amount);
-	void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+	void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 
@@ -26,26 +26,26 @@ class Pay : public Card{
     int amount;
     public:
     Pay (std::string new_text, int new_amount);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Go_to_jail : public Card{
     public:
     Go_to_jail (std::string new_text);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Collect_from_each_player : public Card{
     int amount;
     public:
     Collect_from_each_player(std::string new_text,int new_amount);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Get_out_of_jail_free  : public Card{
   public:
     Get_out_of_jail_free (std::string new_text) ;
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Pay_for_each_residence : public Card {
@@ -53,13 +53,13 @@ class Pay_for_each_residence : public Card {
     int hotel_amount;
     public:
     Pay_for_each_residence (std::string new_text,int new_house_amount,int new_hotel_amount);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 } ;
 
 class Jury_Duty : public Card{
     public:
     Jury_Duty(std::string new_text);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Advance_to : public Card{
@@ -68,12 +68,12 @@ class Advance_to : public Card{
     public:
     Advance_to(std::string new_text, int new_space_pos);
     Advance_to(std::string new_text,std::string new_space_color);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
 };
 
 class Pay_each_player: public Card{
     int amount;
     public:
     Pay_each_player(std::string new_text,int new_amount);
-    void function(Player& player, std::vector<Space*>& spaces_ref, std::vector<Player>& players_ref);
-};
+    void function(Player& player, std::vector<Space*>& spaces, std::vector<Player>& players);
+};*/
