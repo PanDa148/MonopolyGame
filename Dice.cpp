bdd54d68e6e2a869b4ccc667de6c5f0540e2182d
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-06-29 14:56:27 -0400

    Update Dice.cpp

--- Dice.cpp
+++ Dice.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 Dice::Dice()
     :die1(0)
@@ -17,47 +18,10 @@ bool Dice::is_doubles() {
 }
 
 void Dice::roll() {
-std::string input;
-  std::string::iterator letter;
-  int length;
-  std::vector<int> nums;
-  int sum=0;
-  int pos1;
-  int pos2;
-      std::cout << "Press a bunch of random keys to roll\n";
-      std::cin >> input;
-      length = input.length ();
-      while (length<20) {
-          std::cout<<"Please enter an input of at least 20 characters\n";
-          std::cin >> input;
-          length = input.length ();
-      }
-      for (letter=input.begin(); letter!=input.end(); letter++) 
-        {
-          nums.push_back( (int) (*letter) );
-          //std::cout<<(int) (*letter)<<" ";
-        }
-        //std::cout<<"\n";
-      for (int x=0; x<nums.size(); x++) {
-          int& num = nums[x];
-          while (num>20) {
-              num = num/2;
-          }
-          //std::cout<<num<<" ";
-      }
-        //std::cout<<"\n";
-        pos1 = nums[nums.size()/3]-1;
-        pos2 = nums[2*nums.size()/3]-1;
-        for (int i=0; i<nums.size(); i++){
-            int& num = nums[i];
-            while (num>6){
-                num=num/2;
-            }
-            //std::cout<<num<<" ";
-        }
-        //std::cout<<"\n";
-        die1 = nums[pos1];
-        die2 = nums[pos2];
-        //std::cout<<pos1<<" "<<pos2<<"\n"<<roll1<<" "<<roll2<<"\n"<<roll1+roll2<<"\n";
-        //std::cout<<"You rolled: "<<roll1+roll2<<"\n";
+    std::cout<<"Press r to roll\n";
+    std::string input;
+    std::cin >> input;
+    die1=rand() % 6 + 1;
+    die2=rand() % 6 + 1;
+    std::cout<<"You rolled: "<<die1+die2<<"\n";
 }
