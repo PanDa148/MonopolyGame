0efdd0effcb909df86fa7ff3f9933541a3986e31
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-06-29 14:59:16 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,68 +4,23 @@
 #include <cmath>
 #include <algorithm>
 #include "Player.h"
+#include "Property.h"
+#include "Land.h"
 Player::Player(std::string new_name):
     name(new_name),
     cash(15000),
     debt(0),
     in_jail(false),
     position(0),
-    get_out_of_jail_card(false),
-    net_worth(0)
+    get_out_of_jail_card(false)
     {}
 //basic functions
 std::string Player::get_name(){
     return name;
 }
 
-int Player::get_position() {
-    return position;
-}
-void Player::set_position(int new_position) {
-    position = new_position;
-}
-
-void Player::advance_position(int num_spaces) {
-    position = position + num_spaces;
-}
-
-int Player::get_cash(){
-    return cash;
-}
-void Player::add_cash(int new_cash){
-    cash = cash + new_cash;
-}
-int Player::get_debt(){
-    return debt;
-}
-void Player::add_debt(int new_debt) {
-    debt = debt+new_debt;
-}
-
-bool Player::is_in_jail(){
-    return in_jail;
-}
-
-void Player::change_jail_status(bool new_status) {
-    in_jail = new_status;
-}
-
-bool Player::has_get_out_of_jail_card(){
-	return get_out_of_jail_card;
-}
-
-void Player::set_get_out_of_jail_card(bool new_card){
-	get_out_of_jail_card=new_card;
-}
-
-void Player::add_property(int num){
-	properties_owned.push_back(num);
-}
-int Player::get_net_worth(){
-	return net_worth;
-}
-void Player::set_net_worth(int new_net_worth){
-	net_worth=new_net_worth;
+void Player::add_property(){
+	properties_owned.push_back(position);
 }
 
 int Player::get_num_properties_owned(){
@@ -73,4 +28,15 @@ int Player::get_num_properties_owned(){
 }
 int Player::get_property_owned(int index){
 	return properties_owned[index];
+}
+
+int Player::calc_net_worth(std::vector<Space*>& spaces) {
+    int total = cash-debt;
+    for (int x=0; x<properties_owned.size(); x++){
+        Property* p = dynamic_cast<Property*>(spaces[properties_owned[x]]);
+		total+=p->get_value();//(p->get_num_houses())*(p->get_house_val())+(p->get_num_hotels())*(p->get_hotel_val());
+        
+	}
+    net_worth=total;
+    return total;
 }
