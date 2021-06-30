1a95569503c205e95759461cf534828ebdcf6a8b
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-06-30 18:21:56 -0400

    Update Property.cpp

--- Property.cpp
+++ Property.cpp
@@ -7,12 +7,8 @@
 Property::Property(){}
 Property::Property(std::string new_name, int new_value, std::string new_color)
     : Space(new_name)
-    , mortgaged(false)
-    , owner("no one")
     , value(new_value)
     , color(new_color)
-    , num_houses(0)
-    , hotel(false)
     {}
 
 std::string Property::get_color () {
@@ -27,7 +23,6 @@ int Property::get_mortgage_value() {
 }
 
 void Property::show_details() {
-    std::cout<<"e\n";
     std::cout<<"\n"<<name<<"\n";
 	if (mortgaged){
 		std::cout<<"MORTGAGED\n";
@@ -37,8 +32,8 @@ void Property::show_details() {
 int Property::count(std::vector<Space*>& spaces) {
     int c=0;
     for (int x=0; x<spaces.size(); x++){
-        Property* p = dynamic_cast<Property*>(spaces[x]);
-		if (p->owner==owner && p->color==color){
+        Space* p = spaces[x];
+		if (p->owner==owner && p->get_color()==color){
 			c++;
 		}
     }
@@ -50,16 +45,15 @@ int Property::rent(std::vector<Space*>& spaces, int dice_rolled) {
 }
 
 void Property::action(Player& player, std::vector<Space*>& spaces, int dice_rolled, std::vector<Player>& players, std::vector<Card*>& community_chest_cards, std::vector<Card*>& chance_cards) {
-    std::string input=" ";
-	while(input!="y" && input!="n" && input!="rent"){
-		if (owner == "no one"){
-			std::cout<<Property::get_name()<<" is not owned. Would you like to buy it for $"<<Property::get_value()<<"?(y/n)\n";
-			std::cin>>input;
-            if (input=="y"){
-                owner=player.get_name();
-                player.cash-=Property::get_value();
-                player.add_property();
+		if (owner == ""){
+			std::string input=" ";
+            while (input!="y" && input !="n"){
+                std::cout<<get_name()<<" is not owned. Would you like to buy it for $"<<Property::get_value()<<"?(y/n)\n";
+                std::cin>>input;
             }
+                owner=player.get_name();
+                player.cash-=get_value();
+                player.add_property();
 		}
 		else if (owner == player.get_name()){
 			std::cout<<"You own "<<name<<".\n";
@@ -68,16 +62,15 @@ void Property::action(Player& player, std::vector<Space*>& spaces, int dice_roll
 			std::cout<<name<<" is mortgaged.\n";
 		}
 		else {
-			int rent = Property::rent(spaces,dice_rolled);
-			std::cout<<Property::get_name()<<" is owned by "<<owner<<". Rent is $"<<rent<<".\n";
-			player.cash-=rent;
+			int r = rent(spaces,dice_rolled);
+			std::cout<<get_name()<<" is owned by "<<owner<<". Rent is $"<<r<<".\n";
+			player.cash-=r;
             for (int x = 0; x<players.size(); x++){
                 if (players[x].get_name()==name){
-                    players[x].cash+=rent;
+                    players[x].cash+=r;
                     break;
                 }
             }
             std::cout<<"Rent paid to "<<owner<<"\n";
 		}
-	}
 }
