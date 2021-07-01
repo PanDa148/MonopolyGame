dff95c16962e4b0348bfca4739872faa0b1dbae6
Dhivya Iyer <51975355+PanDa148@users.noreply.github.com>
2021-07-01 17:44:04 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,41 +1,76 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cmath>
-#include <algorithm>
-#include "Player.h"
 #include "Property.h"
-#include "Land.h"
-Player::Player(std::string new_name):
-    name(new_name),
-    cash(15000),
-    debt(0),
-    in_jail(false),
-    position(0),
-    get_out_of_jail_card(false)
+#include "Player.h"
+
+Property::Property(){}
+Property::Property(std::string new_name, int new_value, std::string new_color)
+    : Space(new_name)
+    , value(new_value)
+    , color(new_color)
     {}
-//basic functions
-std::string Player::get_name(){
-    return name;
+
+std::string Property::get_color () {
+    return color;
 }
 
-void Player::add_property(){
-	properties_owned.push_back(position);
+int Property::get_value() {
+    return value;
+}
+int Property::get_mortgage_value() {
+    return value/2;
 }
 
-int Player::num_properties_owned(){
-	return properties_owned.size();
-}
-int Player::get_property_owned(int index){
-	return properties_owned[index];
-}
-
-int Player::calc_net_worth(std::vector<Space*>& spaces) {
-    int total = cash-debt;
-    for (int x=0; x<properties_owned.size(); x++){
-        Space* s = spaces[properties_owned[x]];
-		total+=s->get_value()+(s->num_houses)*(s->get_house_val())+(s->hotel)*(s->get_hotel_val());
+void Property::show_details() {
+    std::cout<<"\n"<<name<<"\n";
+	if (mortgaged){
+		std::cout<<"MORTGAGED\n";
 	}
-    net_worth=total;
-    return total;
+}
+
+int Property::count(GameBoard& gb) {
+    int c=0;
+    for (int x=0; x<40; x++){
+        Space* p = gb[x];
+		if (p->owner==owner && p->get_color()==color){
+			c++;
+		}
+    }
+    return c;
+}
+
+int Property::rent(GameBoard& gb) {
+    return 0;
+}
+
+void Property::action(Player& player, GameBoard& gb, std::vector<Player>& players) {
+		if (owner == ""){
+			std::string input=" ";
+            while (input!="y" && input !="n"){
+                std::cout<<get_name()<<" is not owned. Would you like to buy it for $"<<Property::get_value()<<"?(y/n)\n";
+                std::cin>>input;
+            }
+                owner=player.get_name();
+                player.cash-=get_value();
+                player.add_property();
+		}
+		else if (owner == player.get_name()){
+			std::cout<<"You own "<<name<<".\n";
+		}
+		else if (mortgaged) {
+			std::cout<<name<<" is mortgaged.\n";
+		}
+		else {
+			int r = rent(gb);
+			std::cout<<get_name()<<" is owned by "<<owner<<". Rent is $"<<r<<".\n";
+			player.cash-=r;
+            for (int x = 0; x<players.size(); x++){
+                if (players[x].get_name()==name){
+                    players[x].cash+=r;
+                    break;
+                }
+            }
+            std::cout<<"Rent paid to "<<owner<<"\n";
+		}
 }
